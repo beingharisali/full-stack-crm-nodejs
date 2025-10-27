f3764c152076b67f2c1661543508bd58cfee5de7
devzain956 <baqirzain449@gmail.com>
2025-10-27 15:33:36 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res)=>{
+    res.status(200).json({
+        success:true,
+        msg:"API WORKING"
+    })
+})
+
+app.listen(port, ()=>{
+    console.log(`Application is up and listening on port ${port}`)
+})
