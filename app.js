a87da1d7aa57dd5ddeae29703823c3f53b1e70b2
Shahzaib Fayyaz <shahzaibdev797@gmail.com>
2025-10-28 14:42:48 +0500

    error resolved
    Fix typo in require statement for CORS module.

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const connectDB = require("./database/connect.js");
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
-
+const cors = requrie ("cors")
 connectDB();
 
+app.use(express.json());
 app.get("/", (req, res) => {
 	res.status(200).json({
 		success: true,
