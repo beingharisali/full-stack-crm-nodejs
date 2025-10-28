c70e2f9d2b1e44b30c31132f995f018a0384dce5
Shahzaib Fayyaz <shahzaibdev797@gmail.com>
2025-10-28 14:09:53 +0500

    setup backend express and connected to database

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
+require("dotenv").config();
+const connectDB = require("./database/connect.js");
+const express = require("express");
+const app = express();
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res)=>{
-    res.status(200).json({
-        success:true,
-        msg:"API WORKING"
-    })
-})
+connectDB();
 
-app.listen(port, ()=>{
-    console.log(`Application is up and listening on port ${port}`)
-})
+app.get("/", (req, res) => {
+	res.status(200).json({
+		success: true,
+		msg: "API WORKING",
+	});
+});
+
+app.listen(port, () => {
+	console.log(`Application is up and listening on port ${port}`);
+});
