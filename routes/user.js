bf8870b832951607e126ccfa62903745a52b11ad
Maroof <maroofahamd076@gamil.com>
2025-10-29 14:55:21 +0500

    secure routes created

--- routes/user.js
+++ routes/user.js
@@ -2,14 +2,10 @@ const express = require("express");
 const router = express.Router();
 const { register, login } = require("../controllers/user");
 const fs = require("fs");
-router.use("/register", register, (req, res, next) => {
+const loginRoute = (req, res, next) => {
   fs.appendFile("logs.txt", `${Date.now()}:${req.method}:${req.path}`);
   next();
-});
-router.use("/login", login, (req, res, next) => {
-  fs.appendFile("logs.txt", `${Date.now()}:${req.method}:${req.path}`);
-  next();
-});
-// router.post("/register", register);
-// router.post("/login", login);
+};
+router.post("/register", register, loginRoute);
+router.post("/login", login, loginRoute);
 module.exports = router;
