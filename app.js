d192d2bef68a744f40df766fbea5190a911d8e1a
Maroof <maroofahamd076@gamil.com>
2025-10-29 13:58:45 +0500

    secure routes

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 5000;
+const userRoutes = require("./routes/user");
+app.use(cors());
+app.use(express.json());
+app.use("/api/v1", userRoutes);
 
-app.get('/', (req, res)=>{
-    res.status(200).json({
-        success:true,
-        msg:"API WORKING"
-    })
-})
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    msg: "API WORKING",
+  });
+});
 
-app.listen(port, ()=>{
-    console.log(`Application is up and listening on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Application is up and listening on port ${port}`);
+});
