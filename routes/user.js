7dfc96de9741c5906db791e1f4cf08f0d7bfb5ca
beingharisali <theharis99@gmail.com>
2025-10-30 15:17:12 +0500

    added middleware and error handlers

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const { register, login } = require("../controllers/user");
-const validate = require("../middleware/user");
 router.post("/register", register);
-router.post("/login", validate, login);
+router.post("/login", login);
 module.exports = router;
