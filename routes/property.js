ea5d9f2c4cf278ba461e81710e116aeba25651cd
Maroof <maroofahamd076@gamil.com>
2025-11-12 13:30:18 +0500

    change in property  model

--- routes/property.js
+++ routes/property.js
@@ -1,9 +1,9 @@
 const {
-	getProperty,
-	createProperty,
-	deleteProperty,
-	editProperty,
-	getSingleProperty,
+  getProperty,
+  createProperty,
+  deleteProperty,
+  editProperty,
+  getSingleProperty,
 } = require("../controllers/property");
 const express = require("express");
 const router = express.Router();
@@ -14,6 +14,6 @@ router.post("/create-property", createProperty);
 router.get("/get-property", getProperty);
 router.patch("/edit-property/:id", editProperty);
 router.delete("/delete-property/:id", deleteProperty);
-router.get("get-single-property/:id", getSingleProperty);
+router.get("/get-single-property/:id", getSingleProperty);
 
 module.exports = router;
